7c5985084227e8a7198fea276e2268cf686858c6
xmal0x <malo2dis@gmail.com>
2016-03-28 22:25:32 +0300

    construct

--- snake/snake/Point.cs
+++ snake/snake/Point.cs
@@ -12,6 +12,17 @@ namespace snake
         public int y;
         public char sym;
 
+        public Point()
+        {
+        }
+        public Point(int x, int y, char sym)
+        {
+            this.x = x;
+            this.y = y;
+            this.sym = sym;
+        }
+
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
