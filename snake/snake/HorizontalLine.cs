17ce5159676a4a4b4742b81806c7bc5b9586cc3a
xmal0x <malo2dis@gmail.com>
2016-03-31 22:39:30 +0300

    line

---
+++ snake/snake/HorizontalLine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class HorizontalLine
+    {
+        List<Point> pList;
+        
+        public HorizontalLine(int xLeft, int xRight, int y, char sym)
+        {
+            pList = new List<Point>();
+            for (int x = xLeft; x <= xRight; x++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);                
+            }
+        }
+        public void Draw()
+        {
+            foreach (Point point in pList)
+            {
+                point.Draw();
+            }
+        } 
+    }
+}
---
+++ snake/snake/VerticalLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class VerticalLine
+    {
+        List<Point> pList;
+
+        public VerticalLine(int x, int yLeft, int yRight, char sym)
+        {
+            pList = new List<Point>();
+            for(int y = yLeft; y <= yRight; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+        public void Draw()
+        {
+            foreach (Point point in pList)
+            {
+                point.Draw();
+            }
+        }
+
+    }
+}
